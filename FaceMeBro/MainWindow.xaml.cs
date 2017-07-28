fc15811571eabb55f553e5733230ef02ba54e184
Kallyn Gowdy <kallyngowdy@gmail.com>
2017-07-27 22:52:28 -0400

    Get basic face detection working

--- FaceMeBro/MainWindow.xaml.cs
+++ FaceMeBro/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using Microsoft.Win32;
 
 namespace FaceMeBro
 {
@@ -20,9 +25,130 @@ namespace FaceMeBro
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string SubscriptionKey = "7f1370c7263d460ca586ed747aa261aa";
+        private const string Endpoint = "https://eastus2.api.cognitive.microsoft.com/face/v1.0";
+
+        private static readonly Dictionary<string, SolidColorBrush> emotionMap = new Dictionary<string, SolidColorBrush>()
+        {
+            {
+                "Anger",
+                Brushes.Red
+            },
+            {
+                "Contempt",
+                Brushes.Black
+            },
+            {
+                "Disgust",
+                Brushes.DarkOliveGreen
+            },
+            {
+                "Fear",
+                Brushes.DarkRed
+            },
+            {
+                "Happiness",
+                Brushes.Yellow
+            },
+            {
+                "Neutral",
+                Brushes.White
+            },
+            {
+                "Sadness",
+                Brushes.Blue
+            },
+            {
+                "Surprise",
+                Brushes.LawnGreen
+            }
+        };
+
+        private readonly FaceServiceClient faceClient = new FaceServiceClient(SubscriptionKey, Endpoint);
+        private string selectedFilePath;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BrowseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                DefaultExt = ".jpg",
+                Filter = "Photos (*.jpeg,*.png,*.gif)|*.jpeg;*.png;*.jpg;*.gif"
+            };
+
+            bool? result = dialog.ShowDialog(this);
+
+            if (result.HasValue && result.Value)
+            {
+                selectedFilePath = dialog.FileName;
+                var uri = new Uri(selectedFilePath);
+                var img = new BitmapImage(uri);
+                OutputImage.Source = img;
+                OutputCanvas.Width = img.Width;
+                OutputCanvas.Height = img.Height;
+                ProgressLabel.Text = "Loading Faces...";
+                Progress.Visibility = Visibility.Visible;
+                ClearFaces();
+                Task.Run(() => DetectFaces());
+            }
+        }
+
+        private async void DetectFaces()
+        {
+            Face[] faces;
+            using (FileStream stream = File.OpenRead(selectedFilePath))
+            {
+                faces = await faceClient.DetectAsync(
+                    stream,
+                    returnFaceId: true,
+                    returnFaceLandmarks: true,
+                    returnFaceAttributes: new[] { FaceAttributeType.Emotion });
+            }
+
+            Dispatcher.Invoke(() =>
+            {
+                foreach (var face in faces)
+                {
+                    // Draw the face rectangle on the output canvas.
+                    var rect = CalculateRectForFace(face);
+                    OutputCanvas.Children.Add(rect);
+                    Canvas.SetTop(rect, face.FaceRectangle.Top);
+                    Canvas.SetLeft(rect, face.FaceRectangle.Left);
+                }
+
+                ProgressLabel.Text = "Wating for user input...";
+                Progress.Visibility = Visibility.Collapsed;
+            });
+        }
+
+        private void ClearFaces()
+        {
+            if (OutputCanvas.Children.Count > 1)
+            {
+                OutputCanvas.Children.RemoveRange(1, OutputCanvas.Children.Count - 1);
+            }
+        }
+
+        private static Rectangle CalculateRectForFace(Face face)
+        {
+            return new Rectangle()
+            {
+                Height = face.FaceRectangle.Height,
+                Width = face.FaceRectangle.Width,
+                Fill = Brushes.Transparent,
+                Stroke = CalculateColorForFace(face),
+                StrokeThickness = 20
+            };
+        }
+
+        private static SolidColorBrush CalculateColorForFace(Face face)
+        {
+            var bestEmotion = face.FaceAttributes.Emotion.ToRankedList().First();
+            return emotionMap[bestEmotion.Key];
         }
     }
 }
